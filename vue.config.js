6d7d41fa2c140b3b149d3efc2e26eb4972695245
yf-MTZ <yangfan@maotuanzi.com>
2019-05-31 16:19:40 +0800

    修改注释

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,17 @@ const glob = require("glob")
 const modulesSrcList = glob.sync("./src/modules/*/main.js")// 获取所有子项目入口路径
 const log = require("./tools/log.js")
 const serveParameter = JSON.parse(process.env.npm_config_argv).cooked[2]// 获取serve运行时候的--参数
-const moduleNameList = modulesSrcList.map((src) => {
+const moduleNameList = modulesSrcList.map((src) => {// 获取所有项目名称列表
 	return (src.split("/"))[3]
-})// 获取所有项目名称列表
-const serveModuleName = serveParameter ? serveParameter.replace(/^-+/g, "") : log.error("请输入要运行的项目名称参数")
+})
+const serveModuleName = serveParameter ? serveParameter.replace(/^-+/g, "") : log.error("请输入要运行的项目名称参数")// 获取运行时的项目名称
 if (process.env.NODE_ENV !== "production") {// 开发环境判断参数是否存在于项目名称列表
 	moduleNameList.indexOf(`${serveModuleName}`) === -1 ? log.error(`${serveParameter}项目不存在`)
 			: log.state(serveModuleName, "service start")
 }
 const pagesConfig = (() => {// 获取项目多页面配置
-	let pages = {}// 打包单独生成每个文件夹的html入口 只有index项目运行全部子项目 其他项目只运行自己的代码
-	if (process.env.NODE_ENV === "production" || `${serveModuleName}` !== "index") {
+	let pages = {}
+	if (process.env.NODE_ENV === "production" || `${serveModuleName}` !== "index") {// build时或serve项目不为index时,只配置单页面模式
 		pages[serveModuleName] = {
 			entry: `src/modules/${serveModuleName}/main.js`,
 			template: `src/modules/${serveModuleName}/${serveModuleName}.html`,
@@ -20,7 +20,7 @@ const pagesConfig = (() => {// 获取项目多页面配置
 			chunks: ["chunk-vendors", "chunk-common", serveModuleName]
 		}
 	}
-	else {
+	else { // index页面运行时配置为多页面方面预览
 		moduleNameList.map((fileName) => {
 			pages[fileName] = {
 				entry: `src/modules/${fileName}/main.js`,
@@ -33,7 +33,7 @@ const pagesConfig = (() => {// 获取项目多页面配置
 	return pages
 })()
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production" ? `` : `/${serveModuleName}/${serveModuleName}.html`,
+	publicPath: process.env.NODE_ENV === "production" ? `` : `/${serveModuleName}/${serveModuleName}.html`,// 修正项目运行的url为子项目路径
 	devServer: {
 		port: 8080,
 		host: "localhost",
@@ -42,7 +42,7 @@ module.exports = {
 		open: true,
 		index: `${serveModuleName}.html`
 	},
-	productionSourceMap: false, // 生产环境map映射 默认true压缩打包后的文件为一行格式
+	productionSourceMap: false, // 禁止产生map文件,调试开启
 	outputDir: `dist/${serveModuleName}`,
 	pages: pagesConfig,
 	chainWebpack: config => { // 覆盖copy插件的默认配置 子项目的public单独进行打包
